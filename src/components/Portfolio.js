bebe8050e4b91f6c78235bd75cdba61089d3b6de
Shubham Verma <shubhamv2010@gmail.com>
2024-05-28 11:38:51 +0530

    finishing touch

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -62,6 +62,7 @@ export default function Portfolio() {
           </div> */}
 
           <ul className="project-list">
+            {/* Portfolio */}
             <li className="project-item  active" data-filter-item data-category="web development">
               <a target="_blank" href="https://shubhamhere.netlify.app">
                 <figure className="project-img">
@@ -77,7 +78,7 @@ export default function Portfolio() {
                 <p className="project-category">Web development</p>
               </a>
             </li>
-
+            {/* bhoji */}
             <li className="project-item  active" data-filter-item data-category="web development">
               <a target="_blank" href="https://bhoji.netlify.app">
                 <figure className="project-img">
@@ -89,118 +90,6 @@ export default function Portfolio() {
                 </figure>
 
                 <h3 className="project-title">Bhoji Enterprises</h3>
-
-                <p className="project-category">Web development</p>
-              </a>
-            </li>
-
-            <li className="project-item  active" data-filter-item data-category="web design">
-              <a href="#">
-                <figure className="project-img">
-                  <div className="project-item-icon-box">
-                    <ion-icon name="eye-outline"></ion-icon>
-                  </div>
-
-                  <img src="./assets/images/project-3.jpg" alt="fundo" loading="lazy" />
-                </figure>
-
-                <h3 className="project-title">Fundo</h3>
-
-                <p className="project-category">Web design</p>
-              </a>
-            </li>
-
-            <li className="project-item  active" data-filter-item data-category="applications">
-              <a href="#">
-                <figure className="project-img">
-                  <div className="project-item-icon-box">
-                    <ion-icon name="eye-outline"></ion-icon>
-                  </div>
-
-                  <img src="./assets/images/project-4.png" alt="brawlhalla" loading="lazy" />
-                </figure>
-
-                <h3 className="project-title">Brawlhalla</h3>
-
-                <p className="project-category">Applications</p>
-              </a>
-            </li>
-
-            <li className="project-item  active" data-filter-item data-category="web design">
-              <a href="#">
-                <figure className="project-img">
-                  <div className="project-item-icon-box">
-                    <ion-icon name="eye-outline"></ion-icon>
-                  </div>
-
-                  <img src="./assets/images/project-5.png" alt="dsm." loading="lazy" />
-                </figure>
-
-                <h3 className="project-title">DSM.</h3>
-
-                <p className="project-category">Web design</p>
-              </a>
-            </li>
-
-            <li className="project-item  active" data-filter-item data-category="web design">
-              <a href="#">
-                <figure className="project-img">
-                  <div className="project-item-icon-box">
-                    <ion-icon name="eye-outline"></ion-icon>
-                  </div>
-
-                  <img src="./assets/images/project-6.png" alt="metaspark" loading="lazy" />
-                </figure>
-
-                <h3 className="project-title">MetaSpark</h3>
-
-                <p className="project-category">Web design</p>
-              </a>
-            </li>
-
-            <li className="project-item  active" data-filter-item data-category="web development">
-              <a href="#">
-                <figure className="project-img">
-                  <div className="project-item-icon-box">
-                    <ion-icon name="eye-outline"></ion-icon>
-                  </div>
-
-                  <img src="./assets/images/project-7.png" alt="summary" loading="lazy" />
-                </figure>
-
-                <h3 className="project-title">Summary</h3>
-
-                <p className="project-category">Web development</p>
-              </a>
-            </li>
-
-            <li className="project-item  active" data-filter-item data-category="applications">
-              <a href="#">
-                <figure className="project-img">
-                  <div className="project-item-icon-box">
-                    <ion-icon name="eye-outline"></ion-icon>
-                  </div>
-
-                  <img src="./assets/images/project-8.jpg" alt="task manager" loading="lazy" />
-                </figure>
-
-                <h3 className="project-title">Task Manager</h3>
-
-                <p className="project-category">Applications</p>
-              </a>
-            </li>
-
-            <li className="project-item  active" data-filter-item data-category="web development">
-              <a href="#">
-                <figure className="project-img">
-                  <div className="project-item-icon-box">
-                    <ion-icon name="eye-outline"></ion-icon>
-                  </div>
-
-                  <img src="./assets/images/project-9.png" alt="arrival" loading="lazy" />
-                </figure>
-
-                <h3 className="project-title">Arrival</h3>
 
                 <p className="project-category">Web development</p>
               </a>
